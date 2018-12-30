1995abbfd122fd651c4a37c3c11e1661219fd907
Raul Dragoiu <raul.dragoiu@in-tech.com>
2018-12-30 17:15:17 +0200

    backend prj

---
+++ nodebackup_backend/nodebackup_backend/src/main/java/com/dragoiu/nodebackup_backend/NodebackupBackendApplication.java
@@ -0,0 +1,14 @@
+package com.dragoiu.nodebackup_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodebackupBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodebackupBackendApplication.class, args);
+	}
+
+}
+
---
+++ nodebackup_backend/nodebackup_backend/src/test/java/com/dragoiu/nodebackup_backend/NodebackupBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dragoiu.nodebackup_backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NodebackupBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
